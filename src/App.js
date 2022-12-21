671f2d9e2a29c4f58d4db7e0e6d0070b9cb989e0
rifat32 <rifatbilalphilips@gmail.com>
2022-12-21 11:00:35 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { GLTFModel, AmbientLight, DirectionLight } from "react-3d-viewer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const modelPath =
+  "https://raw.githubusercontent.com/dwqdaiwenqi/react-3d-viewer/master/site/src/lib/model/DamagedHelmet.gltf";
+return (
+  <div style={{}}>
+    Rifat 
+    <GLTFModel src={modelPath} >
+      <AmbientLight color={0xffffff} />
+      <DirectionLight
+        color={0xffffff}
+        position={{ x: 100, y: 200, z: 100 }}
+      />
+      <DirectionLight
+        color={0xff00ff}
+        position={{ x: -100, y: 200, z: -100 }}
+      />
+    </GLTFModel>
+  
+  </div>
   );
 }
 
